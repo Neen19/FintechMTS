5643cc30bc112a796b8f1d4107f061f472f30354
Sarmosov Nikita <nicitasarmosov@yandex.ru>
2024-03-24 15:12:09 +0300

    thread safe collections

--- spring-boot/demo-starter/src/main/java/ru/mtsbank/demofintech/utils/MapUtils.java
+++ spring-boot/demo-starter/src/main/java/ru/mtsbank/demofintech/utils/MapUtils.java
@@ -3,15 +3,16 @@ package ru.mtsbank.demofintech.utils;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 public class MapUtils {
     public static <V> void putInMap(Map<String, List<V>> map, V elem) {
         String key = elem.getClass().getName();
         List<V> list = map.get(key);
         if (list == null) {
-            List<V> arrayList = new ArrayList<>();
-            arrayList.add(elem);
-            map.put(key, arrayList);
+            List<V> safeList = new CopyOnWriteArrayList<>();
+            safeList.add(elem);
+            map.put(key, safeList);
         }
         else list.add(elem);
     }
